ec8d1b541e93270a398888b8e653b35ca235b37b
Gaurav Juvekar <gauravjuvekar@gmail.com>
2020-12-15 19:23:44 -0800

    Make symbolic links by default

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ set -Eeuxo pipefail
 for file in .bash* .screenrc;
 do
     rm -f "$HOME/$(basename $file)"
-    ln -T "$file" "$HOME/$(basename $file)"
+    ln -rs -T "$file" "$HOME/$(basename $file)"
 done
