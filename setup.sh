bf20fe7672f63b9a2ec26f205a65db438def8c6b
Gaurav Juvekar <gauravjuvekar@gmail.com>
2022-09-13 23:29:46 -0700

    Setup .env

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuxo pipefail
 
-for file in .bash* .screenrc;
+for file in .bash* .screenrc .env ;
 do
     rm -f "$HOME/$(basename $file)"
     ln -rs -T "$file" "$HOME/$(basename $file)"
